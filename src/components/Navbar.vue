63f4ff6dcefdd490a93b5c3cc225ab34a987b50c
Andres Oliveira <andresoliveira@protonmail.com>
2024-07-22 11:35:23 -0300

    add: login and signup design

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">
+      <img src="../assets/logo.svg" width="24px"/>
+      Vue Auth
+    </a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li> -->
+      </ul>
+      <form class="d-flex align-items-center justify-content-center gap-2">
+        <router-link to="/login">
+          <button class="btn btn-outline-success" type="submit">Login</button>
+        </router-link>
+        <router-link to="/signup">
+          <button class="btn btn-outline-success" type="submit">Signup</button>
+        </router-link>
+      </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col-md-6 offset-md-3 offset-lg-3">
+      <div>
+        <div>
+          <h3>Signup</h3>
+          <hr />
+        </div>
+        <form>
+          <div class="form-group">
+            <label>Email</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="my-3">
+            <button type="submit" class="btn btn-primary">Singup</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
